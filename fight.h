0f8559cf0ded7993f3b4a7c99e1f491863d9a203
Jim Ries <RiesJ@missouri.edu>
2024-11-08 15:04:35 -0600

    Inital roll-in

---
+++ fight.h
@@ -0,0 +1,21 @@
+#ifndef _fight_H
+#define _fight_H
+
+// Types
+struct _Combatant
+{
+    char name[256];
+    int ac;
+    int hp;
+    int hitbonus;
+    char attack1damage[256];
+    char attack2damage[256];
+    int crit;
+};
+typedef struct _Combatant Combatant;
+
+// Prototypes
+void Fight(Combatant combatant1, Combatant combatant2);
+void SetSeed(unsigned int seed);
+
+#endif
